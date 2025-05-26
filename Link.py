8f6c0c08a8ae10721ffa9c9dcaa9025ca9d2d885
Taha Tehrani Nasab <79411820+tahatehran@users.noreply.github.com>
2025-05-26 20:39:17 +0330

    Create Link.py

---
+++ Link.py
@@ -0,0 +1,7 @@
+LIARA_API_PATHS = [
+    "682bb6c5009ad8b8440289b4",
+    "682bb8eb153623bd82f7d38e",
+    # Add more API paths here as needed
+]
+
+LIARA_BASE_URLS = [f"https://ai.liara.ir/api/v1/{path}" for path in LIARA_API_PATHS]
