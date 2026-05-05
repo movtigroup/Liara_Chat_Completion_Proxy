50795cf8049b7dd1681f74cd4fec77228d100226
Taha Tehrani Nasab <115104136+tahatehranin@users.noreply.github.com>
2026-05-05 14:29:33 +0330

    Update link.py

--- link.py
+++ link.py
@@ -1,6 +1,6 @@
 LIARA_API_PATHS = [
-    "682bb8eb153623bd82f7d38e",
-    "682bb6c5009ad8b8440289b4"
+    "682bb800000000",
+    "682bb600000000"
 ]
 
-LIARA_BASE_URLS = [f"https://ai.liara.ir/api/v1/{path}" for path in LIARA_API_PATHS]
+LIARA_BASE_URLS = [f"https://ai.liara.ir/api/{path}/v1" for path in LIARA_API_PATHS]
